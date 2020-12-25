7dd5c29a23fde3a67b3202f6b8e7bad3eb464173
sinework <sine.s.amare@gmail.com>
2020-12-25 12:21:31 +0300

    Add migration

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
 class Article < ApplicationRecord
+
+    belongs_to :user, class_name: 'User', foreign_key: :Author_id
+    has_many :votes, dependent: :destroy
+    has_many :tags, dependent: :destroy
+    has_many :categories, through: :tags
 end
