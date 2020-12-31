ed0bb3daa27a2cb0ec3e890c7d3a3536169766c1
sinework <sine.s.amare@gmail.com>
2020-12-31 22:35:55 +0300

    Add validation and associations to the category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,10 @@
 class Category < ApplicationRecord
-    
-    has_many :articles
     has_many :tags
+    has_many :articles, through: :tags
+    
+    
+    scope :most_recent, ->(category) { find(category.id).articles.order(created_at: :desc).limit(1).first }
+
+    scope :top_priority, ->(limit = nil) { all.order(priority: :desc).limit(limit) }
+  
 end
