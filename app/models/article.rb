e6a239e82017a7a41d80f10b3400ffc02526dc35
sinework <sine.s.amare@gmail.com>
2020-12-31 22:35:24 +0300

    Add validation and associations to the article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,13 @@
 class Article < ApplicationRecord
 
-    belongs_to :user, class_name: 'User', foreign_key: :Author_id
+    belongs_to :user, class_name: 'User', foreign_key: :author_id
     has_many :votes, dependent: :destroy
     has_many :tags, dependent: :destroy
     has_many :categories, through: :tags
     has_one_attached :image
+    scope :top_voted, -> { order(vote_count: :desc).limit(1).first }
+    scope :articles_by_user, ->(current_user_id) { where('"AuthorId" = ?', current_user_id) }
+    scope :search, ->(search) { where('text ILIKE ?', "%#{search}%") }
+    scope :include_votes, -> { includes(:votes).order(created_at: :desc) }
+  
 end
